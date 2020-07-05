b3fda3154d2e8c4f91e5a408f7ac9b6b123f660d
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-07-05 16:22:01 +0300

    lection routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import Todo from './components/Todo';
+import About from './components/About';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+    Link,
+} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
     return (
-        <>
-            <header>Todo APP</header>
-            <Todo />
-        </>
+        <Router>
+            <header>
+                My APP
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>{' '}
+                    </li>
+                    <li>
+                        <Link to="/todo">Todos</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </header>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/todo">
+                    <Todo />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="*">
+                    <Redirect to="/" />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About() {
+    return <div>About PAGE</div>;
+}
+
+export default About;
